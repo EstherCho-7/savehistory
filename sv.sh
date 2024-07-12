3aaea9c05caafed301a8778d52a506ab6adcc347
EstherCho-7 <dkgkqm777@gmail.com>
2024-07-12 11:00:47 +0900

    악

--- sv.sh
+++ sv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-YYMMDDHHMMSS=$(date +"%y%m%d%H%M%S")
+DATE=$(date +"%y%m%d%H%M%S")
 
-history | cut -d ' ' -f4 | sort | uniq > history_$YYMMDDHHMMSS.log
+history | cut -d ' ' -f4 | sort | uniq > history_$DATE.log
 ~                                                                          
